df9104e6aeddb814d27df7cbd65cfa47ef35a097
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2024-12-05 16:57:03 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,4 +27,22 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log(`Socket Disconnected: ${socket.id}`);
   });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incoming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
 });
